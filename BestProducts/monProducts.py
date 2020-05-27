6bd0cbc69a2079fe15b784a50b2dc27408b2731f
jSebastianAR <jSebastian_AR@outlook.com>
2020-05-27 01:23:11 -0500

    se modularizo bestProducts.py y se creo monProducts.py que será para encontrar los 20 productos más productivos con base en el dinero

--- BestProducts/monProducts.py
+++ BestProducts/monProducts.py
@@ -10,15 +10,20 @@ def giveValues(data_top,year):
 
 	d = {}
 	d['year'] = year
-	for i in range(0,data_top.size):
-		d[data_top[i]] = i+1
+	for i in range(0,data_top[0].size):
+		d[data_top[0][i]] = data_top[1][i]
 	return d	
 
-def getTop(year,data):
-	index = year - 2008
-	data_year = data[::,index]
+def getTop(year,data,index):
+	#index = year - 2008
+	data_product = data[:,index-1]
+	data_cash	 = data[:,index]
+	print(f"product: {data_product} \n\n\n cash: {data_cash} \n")
+	data_year = []
+	data_year.append(data_product)
+	data_year.append(data_cash)
 	#print('\n\nYear %s'%(year))
-	array2save = data_year.reshape((data_year.size,1))
+	#array2save = data_year.reshape((data_year.size,1))
 	#print(array2save)
 	#saveArray(array2save,year)
 	data_top = giveValues(data_year,year)
@@ -57,9 +62,9 @@ def extract_data_money():
 
 	doc = pd.read_excel('Productos Agrícolas.xlsx') #reading the data
 	products = doc.head(10).values
-	print(f"{products.shape} \n\n {products}")
-	products = products[::,::2] #get only the product's name
-	print(f"\n\n{products}")	
+	
+	
+	
 
 	return products
 
@@ -67,7 +72,7 @@ def extract_data():
 	doc = pd.read_excel('Productos Agrícolas.xlsx') #reading the data
 	#products = pd.DataFrame(doc,columns = ['2008','2009','2010','2011','2012','2013','2014','2015','2016','2017','2018'])
 	products = doc.head(100).values
-	products = products[::,::2] #get only the product's name
+	#products = products[::,::2] #get only the product's name
 	#print(products)	
 
 	return products
@@ -78,16 +83,18 @@ def get_all_Top_lists():
 	TOP_LIST = []
 
 	#Get all the top list between 2008-2018
+	index = 1
 	for i in range(2008,2019):
-		TOP_LIST.append(getTop(i,products))
+		TOP_LIST.append(getTop(i,products,index))
+		index += 2
 
 	for element in TOP_LIST:
 		print(element)
 		print()
 
 	#Gets the average position for each product
-	result = getProm(TOP_LIST[0],TOP_LIST)
-	return result
+	#result = getProm(TOP_LIST[0],TOP_LIST)
+	#return result
 
 
 def sort_elements(result):
@@ -114,7 +121,6 @@ def save_top_list(sortedResult):
 	#print(sortedResult.keys())
 
 
-#TOP_LIST = get_all_Top_lists()
-#sortedResult = sort_elements(TOP_LIST)
-#save_top_list(sortedResult)
-extract_data_money()
+
+#extract_data_money()
+get_all_Top_lists()
