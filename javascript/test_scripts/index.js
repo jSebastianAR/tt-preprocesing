7e8eb6bf81fd16fdf07c0b0e408ec035ba08c93b
jSebastianAR <jSebastian_AR@outlook.com>
2020-09-03 22:46:42 -0500

    Agregados modulos para scripts de prueba e index.js usado como main para importar modulos customizados

--- javascript/test_scripts/index.js
+++ javascript/test_scripts/index.js
@@ -1,2 +1,18 @@
-console.log(`Hola`)
+import args from './args.js';
+import evnt from './events_test.js';
+//import * as args from './args.js';
 
+const myList = ['this','is','my list','from index.js'];
+
+
+/*
+args.printElements(myList)
+//printElements(myList)
+args.printElements()
+args.printList(myList)
+*/
+let obj = {'hola':'mundo'}
+evnt.emitter.emit('string','hola mundo');
+evnt.emitter.emit('integer',666);
+evnt.emitter.emit('list',[1,2,3,'666',{'buey':'no'}]);
+evnt.emitter.emit(obj,{'nomanches':'esebuey','soy':'elfedebergas'})
