2e13912fa4ae7fbe318718283861e466ba2ed04b
jSebastianAR <jSebastian_AR@outlook.com>
2020-09-25 20:43:02 -0500

    Se agregaron las pantallas para seleccionar el periodo de tiempo que se quiere llenar de datos en filechooser.py, ademas de que se documentaron archivos como towns.py y filechooser.py

--- WheaterScript/filechooser.py
+++ WheaterScript/filechooser.py
@@ -1,7 +1,11 @@
 import re
+import tkinter as tk
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename #Filechooser for one file
 from tkinter.filedialog import askopenfilenames #Filechooser for multiple files
+from tkcalendar import Calendar, DateEntry
+from tkinter import Label, Button
+from datetime import datetime
 
 regex_lat = r'LATITUD[ ]+:(.*)°'
 regex_lon = r'LONGITUD[ ]+:(.*)°'
@@ -16,23 +20,95 @@ def get_lon_lat(file_path):
 
     with open(file_path,'r') as file:
         content = file.read()
-        match_lat = re.findall(regex_lat,content)
-        match_lon = re.findall(regex_lon,content)
+        match_lat = re.findall(regex_lat,content) #Encuentra la latitud
+        match_lon = re.findall(regex_lon,content) #Encuentra la longitud
         
         lat = get_value('Latitud',match_lat[0])
         lon = get_value('Longitud',match_lon[0])
         #print(f'Lat: {lat} Lon: {lon}')
         return [file_path,lat,lon]
 
+
 class Filechooser(object):
     
     def __init__(self, *args, **kwargs):
-        pass
+        self.date_from = None
+        self.date_to = None
+
+    def grab_date(self,calendar,label,flag):
+        
+        date = calendar.get_date()
+        label.config(text=date)
+        
+        if flag:
+            self.date_from = date
+        else:
+            self.date_to = date
+
+    def calendar(self):
+        
+        
+        root = Tk() #Crea la ventana principal
+        root.title('Get Date')
+        root.geometry('500x700')
+
+        mindate_date = datetime(2008,1,1) #Crea una variable del tipo datetime con param: Y:2008 M:1 D:1
+        maxdate_date = datetime(2019,1,1) #Crea una variable del tipo datetime con param: Y:2019 M:1 D:1
+        
+        #Etiqueta de descripción para cal_from
+        label_from = Label(root,text='Fecha de inicio')
+        label_from.pack(pady=20)
+        #label_from.pack(pady=20,padx=200, fill='x')
+
+        #instancia de calendario para la fecha de inicio de llenado de datos y otra para la fecha de termino
+        cal_from = Calendar(root, selectmode='day',year=2008, month=1, day=1, mindate=mindate_date ,maxdate=maxdate_date)
+        cal_from.pack(pady=20)
+        
+        #Funcion para extraer las fechas seleccionadas
+        grab_date_from_lambda = lambda : self.grab_date(cal_from,label_from,True)
+
+        my_button_from = Button(root,text='Start date', command=grab_date_from_lambda)
+        my_button_from.pack(pady=20)
+
+        #Etiqueta de descripción para cal_to
+
+        label_to = Label(root,text='Fecha de fin')
+        label_to.pack(pady=20)
+        
+        #instancia de calendario para la fecha de termino
+        cal_to = Calendar(root, selectmode='day',year=2008, month=1, day=1, mindate=mindate_date ,maxdate=maxdate_date)
+        cal_to.pack(pady=20)
+        
+        #Funcion para extraer las fechas seleccionadas
+        grab_date_to_lambda = lambda : self.grab_date(cal_to,label_to,False)
+
+        my_button_to = Button(root,text='Final date', command=grab_date_to_lambda)
+        my_button_to.pack(pady=20)
+
+        the_button = Button(root,text='Finish', command = root.destroy)
+        the_button.pack()
+        root.mainloop()
+        
+        return {'from':self.date_from, 'to':self.date_to}
+    """
+    El filechooser permitira elegir el archivo con datos a llenar y los archivos a usar para llenar
+
+    Retornara un diccionario con la siguiente info:
+
+    {'tofill':[path,lat,lon],'touse':[[path,lat,lon,dist],[path,lat,lon,dist], ...]}
+
+    donde: 
+        tofill es la lista con la ciudad de los datos a llenar
+        
+        touse es la lista de listas, donde cada lista indexada es la info de las ciudades
+        a usar para llenar la lista 'tofill'
+    """
 
     def filechooser(self):
 
         #Select files
-        Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+        root = Tk()
+        root.withdraw() # we don't want a full GUI, so keep the root window from appearing
         filename_tofill = askopenfilename(initialdir='./CleanedData', title='Seleccione archivo a llenar información') # show an "Open" dialog box and return the path to the selected file
         files_touse = list(askopenfilenames(initialdir='./CleanedData', title='Seleccione archivos a a usar para llenar información')) # show an "Open" dialog box and return the path to the selected file
 
@@ -43,11 +119,17 @@ class Filechooser(object):
 
         #print(f'Archivo a llenar: {filename_tofill}\n\nArchivos a usar para llenar: {files_touse}\n\n')
         #print(townstouse_latlon,towntofill_latlon)
+        root.destroy()
         return {'tofill':towntofill_latlon,'touse':townstouse_latlon}
 
 
 if __name__ == '__main__':
 
     chooser = Filechooser()
+    
     result = chooser.filechooser()
     print(result)
+    
+    dates = chooser.calendar()
+    print(dates)
+    #root.mainloop()
--- WheaterScript/towns.py
+++ WheaterScript/towns.py
@@ -1,18 +1,29 @@
 from math import sin, cos, sqrt, atan2, radians
+"""
+KEYWORDS:
 
+TF: Town to fill, se refiere a la ciudad de la cual se van a estar llenando datos con respecto
+a otras ciudades
+
+TU: Town to use, se refiere a cada una de las ciudades a usar para llenar los datos faltantes en TF
+"""
+
+
+#Obtiene la distancia entre TF con respecto a las TU
 def get_distances_between_towns(dict_files):
 
-    getDistances_lambda = lambda towntouse: getDistance(dict_files['tofill'][1],dict_files['tofill'][2],towntouse[1],towntouse[2])
-    all_distances = list(map(getDistances_lambda,dict_files['touse']))
+    getDistances_lambda = lambda towntouse: getDistance(dict_files['tofill'][1],dict_files['tofill'][2],towntouse[1],towntouse[2]) #Distancia entre la TF a llenar datos y una TU
+    all_distances = list(map(getDistances_lambda,dict_files['touse'])) #Mapea todas las ciudades a usar para el llenado y calcula las distancias
     
     print(all_distances)
-    appendNewDistance_lambda = lambda current_list,index: current_list.append(all_distances[index])
+    appendNewDistance_lambda = lambda current_list,index: current_list.append(all_distances[index]) #Agrega las distancias que tiene cada TU con TF
     [appendNewDistance_lambda(townlist,dict_files['touse'].index(townlist)) for townlist in dict_files['touse']]
     
     print(f'final data: {dict_files}')
 
     return dict_files
 
+#Obtiene la distancia entre dos lugares dada la latitud y longitud de cada lugar
 def getDistance (latPointA, lonPointA, latPointB, lonPointB):
 
     radiusOfEarth = 6373.0
@@ -32,12 +43,32 @@ def getDistance (latPointA, lonPointA, latPointB, lonPointB):
 
     return round(distance,2)
 
+"""
+Funcion que construye instancias de la clase Towns para la TF y las TU, que hasta el momento
+en que se llama a esta funcion habían estado representadas por listas donde se guardan todos sus datos
+
+Recibe data_towns: un diccionario con las keys: 'tofill' y 'touse' donde:
+
+tofill: guarda en una lista la info de la TF de la siguiente forma
+        [path,lat,lon]
+
+touse: Es una lista de listas, donde cada lista indexada es una TU, su estructura es la sig:
+        [[path,lat,lon,dist],[path,lat,lon,dist], ...]
+
+Donde:
+
+path = ruta completa del archivo de la ciudad que contiene los datos a usar.
+lat = latitud donde se encuentra la ciudad, extraida del archivo señalado por 'path'
+lon = longitud donde se encuentra la ciudad, extraida del archivo señalado por 'path'
+dist = La distancia entre la TU y la TF (Cada TU tiene su propia dist)
+"""
 def build_towns(data_towns):
 
     list_towns = []
-    list_towns.append(Towns(data_towns['tofill'][0],data_towns['tofill'][1],data_towns['tofill'][2],0.0))
+    list_towns.append(Towns(data_towns['tofill'][0],data_towns['tofill'][1],data_towns['tofill'][2],0.0)) #instancia de TF
 
     num_towns = len(data_towns['touse'])
+    #Instancia de cada TU
     for x in range(0,num_towns):
         new_town = Towns(data_towns['touse'][x][0],data_towns['touse'][x][1],data_towns['touse'][x][2],data_towns['touse'][x][3])
         list_towns.append(new_town)
@@ -50,29 +81,32 @@ def getValuesTown(town):
 
 class Towns(object):
     
-    
+    #Constructor
     def __init__(self, path, lat, lon, dist):
         self.path = path
         self.lat = lat
         self.lon = lon
-        self.dist = dist
+        self.dist = dist #Este parámetro tendrá un valor válido para las TU
         self.content = []
         self.name = self.getNameTown(path)
 
+    #Obtiene el nombre respecto al path
     def getNameTown(self,path):
-
         data = path.split('/')
         return data[len(data)-1]
 
+    #Obtendrá cada uno de los datos del archivo de cada town
     def getContent(self):
-        
         dataList = []
-        getDailyData = lambda line: True if ('FECHA' in line) else False
+        #Flag que indicará si ya se puede obtener info
+        getDailyData = lambda line: True if ('FECHA' in line) else False 
+        #Agregará la línea de datos si la bandera obtenida por getDailyData es true
         evaluateLine = lambda line,flag: dataList.append(line) if(flag) else False
         getData = False
 
         with open(self.path,'r',encoding = "ISO-8859-1") as file:
 
+            #Por cada línea de archivo
             for line in file:
 
                 evaluateLine(line,getData)
