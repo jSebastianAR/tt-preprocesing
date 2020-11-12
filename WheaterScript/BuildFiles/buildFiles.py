db520ef4078b58a75447d5ee0a8e0e3fef6911a7
jSebastianAR <jSebastian_AR@outlook.com>
2020-11-11 22:13:20 -0600

    Se termino script buildFiles.py que genera los archivos de cada uno de los municipios faltantes que hay en el archivo missingTowns.txt y se generaron estos archivos con un prefijo que va desde 15001-15042

--- WheaterScript/BuildFiles/buildFiles.py
+++ WheaterScript/BuildFiles/buildFiles.py
@@ -2,6 +2,7 @@ import subprocess
 from pathlib import Path
 import time
 import json
+import dates as dt
 
 MUNICIPIOS = ['ACATIC',
 'AMACUECA',
@@ -17,7 +18,6 @@ MUNICIPIOS = ['ACATIC',
 'CHIMALTITAN',
 'CHIQUILISTLAN',
 'ENCARNACION DE DIAZ',
-'ETZATLAN',
 'EL GRULLO',
 'IXTLAHUACAN DEL RIO',
 'JUANACATLAN',
@@ -47,14 +47,14 @@ MUNICIPIOS = ['ACATIC',
 'ZAPOTLAN DEL REY',
 'SAN IGNACIO CERRO GORDO']
 
-MUNICIPIOS = {'ACATIC': {'lat': '20.780°', 'lon': '-102.91°'},
+MUNICIPIOS_DICT = {'ACATIC': {'lat': '20.780°', 'lon': '-102.910°'},
 'AMACUECA': {'lat': '20.008°', 'lon': '-103.597°'},
 'EL ARENAL': {'lat': '20.769°', 'lon': '-103.695°'},
 'ATEMAJAC DE BRIZUELA': {'lat': '20.139°', 'lon': '-103.726°'},
 'ATENGO': {'lat': '20.274°', 'lon': '-104.235°'},
 'AYOTLAN': {'lat': '20.533°', 'lon': '-102.332°'},
 'AYUTLA': {'lat': '20.040°', 'lon': '-104.432°'},
-'BOLANOS': {'lat': '21.832°', 'lon': '-103.780°'},
+'BOLAÑOS': {'lat': '21.832°', 'lon': '-103.780°'},
 'CASIMIRO CASTILLO': {'lat': '19.341°', 'lon': '-104.287°'},
 'COCULA': {'lat': '20.363°', 'lon': '-103.826°'},
 'CUAUTLA': {'lat': '20.202°', 'lon': '-104.408°'},
@@ -70,45 +70,99 @@ MUNICIPIOS = {'ACATIC': {'lat': '20.780°', 'lon': '-102.91°'},
 'SAN JUAN DE LOS LAGOS': {'lat': '21.250°', 'lon': '-102.329°'},
 'SAN JULIAN': {'lat': '21.005°', 'lon': '-102.178°'},
 'SAN MARCOS': {'lat': '20.791°', 'lon': '-104.198°'},
-'SAN MARTIN DE BOLANOS': {'lat': '21.499°', 'lon': '-103.800°'},
+'SAN MARTIN DE BOLAÑOS': {'lat': '21.499°', 'lon': '-103.800°'},
 'SAN MIGUEL EL ALTO': {'lat': '21.035°', 'lon': '-102.402°'},
 'GOMEZ FARIAS': {'lat': '19.780°', 'lon': '-103.491°'},
 'SAN SEBASTIAN DEL OESTE': {'lat': '20.762°', 'lon': '-104.854°'},
 'SANTA MARIA DE LOS ANGELES': {'lat': '22.175°', 'lon': '-103.226°'},
 'TECALITLAN': {'lat': '19.469°', 'lon': '-103.307°'},
 'TENAMAXTLAN': {'lat': '20.213°', 'lon': '-104.163°'},
-'TLAJOMULCO DE ZUNIGA': {'lat': '20.462°', 'lon': '-103.435°'},
+'TLAJOMULCO DE ZUÑIGA': {'lat': '20.462°', 'lon': '-103.435°'},
 'TLAQUEPAQUE': {'lat': '20.596°', 'lon': '-103.338°'},
 'TONAYA': {'lat': '19.787°', 'lon': '-103.970°'},
 'VALLE DE GUADALUPE': {'lat': '21.011°', 'lon': '-102.620°'},
 'SAN GABRIEL': {'lat': '19.744°', 'lon': '-103.765°'},
 'VILLA CORONA': {'lat': '20.418°', 'lon': '-103.667°'},
 'VILLA HIDALGO': {'lat': '21.675°', 'lon': '-102.587°'},
-'CANADAS DE OBREGON': {'lat': '21.148°', 'lon': '-102.687°'},
+'CAÑADAS DE OBREGON': {'lat': '21.148°', 'lon': '-102.687°'},
 'ZAPOTILTIC': {'lat': '19.629°', 'lon': '-103.427°'},
 'ZAPOTITLAN DE VADILLO': {'lat': '19.547°', 'lon': '-103.810°'},
 'ZAPOTLAN DEL REY': {'lat': '20.465°', 'lon': '-102.924°'},
 'SAN IGNACIO CERRO GORDO' : {'lat': '20.768°', 'lon': '-102.511°'}}
 
-FINAL_LINE_HEADER = 'FECHA'
-INIT_COUNTER = 15000
+ROOT_PATH = '/home/jsebastian-ar/Documentos/git-repos/tt-preprocesing/WheaterScript/CleanedData/'
 
-def get_header_data(self):
-        
-        with open(self.path_origin_file, 'r', encoding = "ISO-8859-1") as file:
-            #print(file)
-            for line in file:
-                self.header.append(line)
-                #print(line)
+FINAL_LINE_HEADER = 'FECHA'
+
+def get_header_data(dict_data):
+        header = []
+        with open('header_esqueleto.txt', 'r', encoding = "utf-8") as file:
+            for line in file:        
+                new_line = check_line_header(line, dict_data)
+                header.append(new_line)
                 if FINAL_LINE_HEADER in line:
                     break
+        return header
 
-def write_file(self,info):
-    with open(self.path_file,'a+', encoding = "ISO-8859-1") as file:
+def check_line_header(line, dict_data):
+    parts = line.split('\n')
+
+    if 'ESTACIÓN' in parts[0]:
+        parts[0] += ' ' + str(dict_data['estacion']) + '\n'
+    elif 'NOMBRE' in parts[0] or 'MUNICIPIO' in parts[0]:
+        parts[0] += ' ' + dict_data['nombre'] + '\n'
+    elif 'LATITUD' in parts[0]:
+        parts[0] += ' ' + dict_data['lat'] + '\n'
+    elif 'LONGITUD' in parts[0]:
+        parts[0] += ' ' + dict_data['lon'] + '\n'
+    else:
+        parts[0] += '\n'
+    return parts[0]
+
+def write_file(path_file,info):
+    with open(path_file,'a+', encoding = "utf-8") as file:
         file.write(info)
 
-def build_path(name_file):
-	root = Path('').resolve().as_posix()
-	final_path = root + '/' + name_file
-	print(final_path)
+def newFile(content,key,dict_town,num_station):
+
+    print(dict_town)
+    path_file = build_path(key,num_station)
+    header = get_header_data({'estacion':num_station,\
+                                'nombre':key,\
+                                'lat': dict_town['lat'],\
+                                'lon': dict_town['lon']})
+    
+    #Writing header info
+    for line in header:
+        write_file(path_file,line)
+
+    #Writing all wheater data
+    for line_list in content:
+        str_line = '    '.join(line_list)
+        write_file(path_file,str_line + '\n')
+    
+    write_file(path_file,'--------------------------------------' + '\n')
+    
+def generate_content():
+    dates_list = dt.generate_date_list('01/01/2008','31/12/2018')
+    content = []
+    for date in dates_list:
+        line = [date,'Nulo','Nulo','Nulo','Nulo']
+        content.append(line)
+    return content
+
+
+def build_path(name_file,num_station):
+	final_path = ROOT_PATH + str(num_station) + '-' + name_file + '.txt'
+	#print(final_path)
 	return final_path
+
+if __name__ == '__main__':
+
+    STATION_COUNTER = 15001
+    content = generate_content()
+    for town in MUNICIPIOS_DICT:
+        print(f'Building file for {town} with {MUNICIPIOS_DICT[town]}')
+        newFile(content,town,MUNICIPIOS_DICT[town],STATION_COUNTER)
+        STATION_COUNTER += 1
+        #time.sleep(3)
