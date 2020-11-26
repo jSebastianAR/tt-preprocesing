ad9e1dcecd192355474908800fa6b75c2d813ec5
jSebastianAR <jSebastian_AR@outlook.com>
2020-11-25 22:32:39 -0600

    Agregada documentacion al script analyzeFakeNulls.py

--- WheaterScript/FakeNulls/analyzeFakeNulls.py
+++ WheaterScript/FakeNulls/analyzeFakeNulls.py
@@ -11,8 +11,8 @@ project_data_path = '/' + parent_path + '/project_data'
 pickles_path = '/' + parent_path + '/Pickles'
 sys.path.append(project_data_path)
 sys.path.append(pickles_path)
-from towns import Towns
-import pickles as pk
+from towns import Towns # MARCA ERROR EN AMBOS IMPORTS DADO QUE SON IMPORTADOS DE OTRO 
+import pickles as pk    # DIRECTORIO, PERO AUN ASÍ FUNCIONA
 
 path_pickles = '/home/jsebastian-ar/Documentos/git-repos/tt-preprocesing/WheaterScript/Pickles/'
 
@@ -26,8 +26,11 @@ def compare_list(original,fake,calculated):
 
 
 def createCSV(list_content):
+    #Columnas del csv
     COLUMNS = ['Archivo','Original','Falso nulo','Calculada','Valor O','Valor C']
+    #Se crea el dataframe con la informacion
     town_df = pd.DataFrame(list_content,columns=COLUMNS)
+    #Se crea el csv
     town_df.to_csv('tabla_fake_nulls.csv')
 
 def analyze_nulls(town,dict_fake_nulls):
@@ -58,31 +61,40 @@ def analyze_nulls(town,dict_fake_nulls):
 
 def set_global_dict_fake_nulls(nulls_list):
     global_dict = {}
+    #Para todos los nulos
     for null in nulls_list:
+        #Obtiene el nombre del archivo
         name_town = null['nombre']
+        #Obtiene el path
         path = null['path']
+        #obtiene los falsos nulos
         all_fakes = null['all_fakes']
+        #Guarda todo en el diccionario de nulos
         global_dict[name_town] = {'path': path, 'all_fakes': all_fakes}
     
     return global_dict
 
 def get_csv_data():
+    #Lee el .csv
     data = pd.read_csv('./tabla_fake_nulls.csv')
+    #Obtiene la columna con los valores originales
     original_value = data['Valor O'].to_numpy()
+    #Obtiene la columna con los valores calculados
     calculated_value = data['Valor C'].to_numpy()
-    #print(data['Valor O'].to_numpy())
-    #print(data['Valor C'].to_numpy())
 
     return original_value, calculated_value
 
 def eficiency():
+    #Obtiene las dos columnas a comparar
     valor_o, valor_c = get_csv_data()
+    #Calcula la diferencia de ambas
     difference = np.subtract(valor_o,valor_c)
-    #print(f'Difference \n {difference}')
+    #Convierte en positivos todos los resultados negativos
     for index in range(0,len(difference)):
         if difference[index] < 0:
             difference[index] = (-1) * difference[index]
     print(f'Difference array\n {difference}')
+    #Calcula el promedio de los resultados obtenidos
     efficiency_mean = round(np.mean(difference),2)
     print(efficiency_mean)
 
@@ -95,18 +107,25 @@ def main():
     dict_fake_nulls = set_global_dict_fake_nulls(fakeNulls_dict)
     #print(dict_fake_nulls)
     final_list_nulls = []
+    #Por cada key
     for key in paths:
         if key<=134:
+            #obtiene el path del archivo
             path_file = paths[key]
+            #Crea una nueva ciudad
             new_town = Towns(path_file,0,0,0)
             print(f'Analizando: {new_town.name}')
+            #Obtiene el contenido de esa ciudad
             new_town.getContent()
+            #Obtiene los indices de las fechas
             new_town.find_indexes_for_dates('01/01/2008','31/12/2018')
+            #Obtiene los nulos falsos agregados a esa ciudad
             fake_nulls = dict_fake_nulls[new_town.name]
+            #Agrega el resultado de comparar los nulos
             final_list_nulls.extend(analyze_nulls(new_town,fake_nulls)) #Agrega la lista de relacion de nulos
         else:
             break
-
+    #Crea el .csv
     createCSV(final_list_nulls)
 if __name__ == '__main__':
     #main()
